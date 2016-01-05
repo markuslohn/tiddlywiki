5b58108053d3bededa6d2709e1ff962398052d7f
Markus Lohn <markus.lohn@esentri.com>
2016-01-05 09:03:46 +0100

    Consolidation.

---
+++ bimalo-common/src/main/java/de/bimalo/common/CommandLineParser.java
@@ -0,0 +1,113 @@
+package de.bimalo.common;
+
+import de.bimalo.common.Assert;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+ * <p>
+ * A parser for arguments provided by a command line interface. The arguments have
+ * follow the syntax:</p>
+ * <p>
+ * <pre>-name=value</pre>
+ * </p>
+ *
+ * @author <a href="mailto:markus.lohn@bimalo.de">Markus Lohn</a>
+ * @version $Rev$ $LastChangedDate$
+ * @since 1.0
+ */
+public final class CommandLineParser {
+
+    /**
+     * The regular pattern describing the syntax of a command line argument:
+     * -parametername=value
+     */
+    private final static String ARGUMENT_PATTERN = "-[\\w]+=[\\w\\W]+";
+
+    /**
+     * Represents java object representing the regular expression describing the
+     * argument syntax.
+     */
+    private Pattern argumentPattern = null;
+
+    /**
+     * A <code>java.util.Map</code> containing all correct parsed arguments from
+     * the command line. The name of the argument is the key and value is the
+     * value provided by the command line.
+     */
+    private Map<String, String> argumentValues = new HashMap<String, String>();
+
+    /**
+     * Creates a new <code>CommandLineParser</code> with default values.
+     *
+     */
+    public CommandLineParser() {
+        argumentPattern = Pattern.compile(ARGUMENT_PATTERN);
+    }
+
+    /**
+     * Parses a given array with arguments provided by the command line
+     * interface.
+     *
+     * @param args array of arguments
+     * @exception IllegalArgumentException if arguments are invalid
+     */
+    public void parseArguments(String[] args) {
+        argumentValues.clear();
+        for (int i = 0; i < args.length; i++) {
+            parseArgument(args[i]);
+        }
+    }
+
+    /**
+     * Gets the value of the argument with the given name.
+     *
+     * @param name the name of the argument
+     * @return value as String if argument exists or null otherwise
+     * @exception IllegalArgumentException if name is null
+     */
+    public String getArgumentValue(String name) {
+        Assert.notNull(name);
+        return argumentValues.get(name);
+    }
+
+    /**
+     * Gets all values retrieved from the command line as unmodifiable
+     * <code>java.util.Map</code>.
+     *
+     * @return argument values as Map.
+     */
+    public Map<String, String> getArgumentValues() {
+        return Collections.unmodifiableMap(argumentValues);
+    }
+
+    /**
+     * Parses an given argument provided by the command line interface.
+     *
+     * @param argument the argument as String
+     * @exception IllegalArgumentException if argument is invalid
+     */
+    private void parseArgument(String argument) {
+        Matcher argumentMatcher = argumentPattern.matcher(argument);
+        if (!argumentMatcher.matches()) {
+            throw new IllegalArgumentException(constructInvalidSyntaxErrorMessage(argument));
+        }
+        int equalsSignIndex = argument.indexOf("=");
+        String argumentName = argument.substring(1, equalsSignIndex);
+        String argumentValue = argument.substring(equalsSignIndex + 1);
+        argumentValues.put(argumentName, argumentValue);
+    }
+
+    private String constructInvalidSyntaxErrorMessage(String argument) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Argument ");
+        sb.append(argument);
+        sb.append(" does not follow the argument syntax ");
+        sb.append("-argumentName=argumentValue.");
+        return sb.toString();
+    }
+
+}
---
+++ bimalo-common/src/test/java/de/bimalo/common/CommandLineParserTest.java
@@ -0,0 +1,103 @@
+package de.bimalo.common;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * <p>
+ * A test case for <code>CommandLineParser</code>.</p>
+ *
+ * @author <a href="mailto:markus.lohn@bimalo.de">Markus Lohn</a>
+ * @since 1.0
+ * @see CommandLineParser
+ */
+public class CommandLineParserTest {
+
+    public CommandLineParserTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void CommandLineParser_RegularExpression() {
+        Pattern p = Pattern.compile("-[\\w]+=[\\w\\W]+");
+        Matcher m = p.matcher("-test=/path/test2.txt");
+        assertEquals(true, m.matches());
+    }
+
+    @Test
+    public void CommandLineParser_parse_OneValidArgument() {
+        String[] args = new String[]{"-rootFolder=/test/test11/xxx"};
+        CommandLineParser parser = new CommandLineParser();
+        parser.parseArguments(args);
+        assertEquals("/test/test11/xxx", parser.getArgumentValue("rootFolder"));
+
+    }
+
+    @Test
+    public void CommandLineParser_parse_TwoValidArgument() {
+        String[] args = new String[]{"-rootFolder=/test/test11/xxx", "-rootFolder2=/test/test22/xxx"};
+        CommandLineParser parser = new CommandLineParser();
+        parser.parseArguments(args);
+        assertEquals("/test/test11/xxx", parser.getArgumentValue("rootFolder"));
+        assertEquals("/test/test22/xxx", parser.getArgumentValue("rootFolder2"));
+    }
+
+    @Test
+    public void CommandLineParser_parse_ValidArgumentContainingWhitespace() {
+        String[] args = new String[]{"-rootFolder=/test/test11 muster/xxx",};
+        CommandLineParser parser = new CommandLineParser();
+        parser.parseArguments(args);
+        assertEquals("/test/test11 muster/xxx", parser.getArgumentValue("rootFolder"));
+    }
+
+    @Test
+    public void CommandLineParser_parse_ArgumentNotAvailable() {
+        CommandLineParser parser = new CommandLineParser();
+        assertNull(parser.getArgumentValue("test"));
+    }
+
+    @Test
+    public void CommandLineParser_parse_InvalidSyntaxInFirstArgument() {
+        String[] args = new String[]{"rootFolder=/test/test11/xxx", "-rootFolder2=/test/test22/xxx"};
+        CommandLineParser parser = new CommandLineParser();
+        try {
+            parser.parseArguments(args);
+            assertTrue("InvalidArgumentException expected.", false);
+        } catch (IllegalArgumentException ex) {
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void CommandLineParser_parse_InvalidSyntaxInSecondArgument() {
+        String[] args = new String[]{"-rootFolder=/test/test11/xxx", "rootFolder2=/test/test22/xxx"};
+        CommandLineParser parser = new CommandLineParser();
+        try {
+            parser.parseArguments(args);
+            assertTrue("InvalidArgumentException expected.", false);
+        } catch (IllegalArgumentException ex) {
+            assertTrue(true);
+        }
+    }
+}
