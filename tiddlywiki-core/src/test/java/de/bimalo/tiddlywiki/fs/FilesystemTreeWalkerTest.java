6cfa21336d3ed749920219878dbaa4a9222b6524
Markus Lohn <markus.lohn@esentri.com>
2017-06-22 17:01:26 +0200

    Re-organized project structure a little bit.

--- tiddlywiki-core/src/test/java/de/bimalo/tiddlywiki/fs/FilesystemTreeWalkerTest.java
+++ tiddlywiki-core/src/test/java/de/bimalo/tiddlywiki/fs/FilesystemTreeWalkerTest.java
@@ -5,8 +5,6 @@ import de.bimalo.tiddlywiki.Tiddler;
 import de.bimalo.tiddlywiki.TiddlyWiki;
 import java.io.IOException;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.apache.commons.vfs2.FileNotFolderException;
 import org.apache.commons.vfs2.FileObject;
 import org.apache.commons.vfs2.FileSystemException;
@@ -26,84 +24,83 @@ import static org.junit.Assert.*;
  */
 public class FilesystemTreeWalkerTest {
 
-    public FilesystemTreeWalkerTest() {
+  public FilesystemTreeWalkerTest() {
+  }
+
+  @BeforeClass
+  public static void setUpClass() {
+  }
+
+  @AfterClass
+  public static void tearDownClass() {
+  }
+
+  @Before
+  public void setUp() {
+  }
+
+  @After
+  public void tearDown() {
+  }
+
+  @Test
+  public void FilesystemTreeWalker_construct_NullArgument() {
+    try {
+      FilesystemTreeWalker tw = new FilesystemTreeWalker(null);
+      fail("A IllegalArgumentException is expected to be thrown.");
+    } catch (IllegalArgumentException ex) {
+      assertTrue(true);
+    } catch (Exception ex) {
+      assertTrue(ex.getMessage(), false);
+    }
+  }
+
+  @Test
+  public void FilesystemTreeWalker_construct_FileObjectDoesNotExist() {
+    try {
+      FileObject file = FileObjectFixture.getDirectoryFileObject("ordner_a", "", false, false);
+
+      FilesystemTreeWalker tw = new FilesystemTreeWalker(file);
+      fail("A IllegalArgumentException is expected to be thrown.");
+    } catch (IllegalArgumentException ex) {
+      assertTrue(true);
+    } catch (Exception ex) {
+      assertTrue(ex.getMessage(), false);
+    }
+  }
+
+  @Test
+  public void FilesystemTreeWalker_construct_FileObjectNotAFolder() {
+    try {
+      FileObject file = FileObjectFixture.getDocumentFileObject("test.txt", "xx", true);
+
+      FilesystemTreeWalker tw = new FilesystemTreeWalker(file);
+      fail("A FileNotFolderException is expected to be thrown.");
+    } catch (FileNotFolderException ex) {
+      assertTrue(true);
+    } catch (Exception ex) {
+      assertTrue(ex.getMessage(), false);
+    }
+  }
+
+  @Test
+  public void FilesystemTreeWalker_walkFileTree_Filesystem1() {
+    try {
+      FileObject rootFolder = FileObjectFixture.getFilesystem1();
+      FilesystemTreeWalker tw = new FilesystemTreeWalker(rootFolder);
+      TiddlyWiki wiki = tw.walkFileTree();
+      assertNotNull(wiki);
+      assertNotNull(wiki.getTitleTiddler());
+      assertNotNull(wiki.getDefaultTiddler());
+
+      List<Tiddler> tiddlers = wiki.listTiddlers();
+      assertTrue(tiddlers.size() >= 1);
+    } catch (FileSystemException ex) {
+      assertTrue(ex.getMessage(), false);
+    } catch (IOException ex) {
+      assertTrue(ex.getMessage(), false);
     }
 
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
-    @Test
-    public void FilesystemTreeWalker_construct_NullArgument() {
-        try {
-            FilesystemTreeWalker tw = new FilesystemTreeWalker(null);
-            fail("A IllegalArgumentException is expected to be thrown.");
-        } catch (IllegalArgumentException ex) {
-            assertTrue(true);
-        } catch (Exception ex) {
-            assertTrue(ex.getMessage(), false);
-        }
-    }
-
-    @Test
-    public void FilesystemTreeWalker_construct_FileObjectDoesNotExist() {
-        try {
-            FileObject file = FileObjectFixture.getDirectoryFileObject("ordner_a", "", false, false);
- 
-            FilesystemTreeWalker tw = new FilesystemTreeWalker(file);
-            fail("A IllegalArgumentException is expected to be thrown.");
-        } catch (IllegalArgumentException ex) {
-            assertTrue(true);
-        } catch (Exception ex) {
-            assertTrue(ex.getMessage(), false);
-        }
-    }
-
-    @Test
-    public void FilesystemTreeWalker_construct_FileObjectNotAFolder() {
-        try {
-            FileObject file = FileObjectFixture.getDocumentFileObject("test.txt", "xx", true);
- 
-            FilesystemTreeWalker tw = new FilesystemTreeWalker(file);
-            fail("A FileNotFolderException is expected to be thrown.");
-        } catch (FileNotFolderException ex) {
-            assertTrue(true);
-        } catch (Exception ex) {
-            assertTrue(ex.getMessage(), false);
-        }
-    }
-    
-    @Test
-    public void FilesystemTreeWalker_walkFileTree_Filesystem1() {
-        try {
-            FileObject rootFolder = FileObjectFixture.getFilesystem1();
-            FilesystemTreeWalker tw = new FilesystemTreeWalker(rootFolder);
-            TiddlyWiki wiki = tw.walkFileTree();
-            assertNotNull(wiki);
-            assertNotNull(wiki.getMainMenuTiddler());
-            assertNotNull(wiki.getTitleTiddler());
-            assertNotNull(wiki.getDefaultTiddler());
-            
-            List<Tiddler> tiddlers = wiki.listTiddlers();
-            assertEquals(1, tiddlers.size());
-        } catch (FileSystemException ex) {
-            assertTrue(ex.getMessage(), false);
-        } catch (IOException ex) {
-            assertTrue(ex.getMessage(), false);
-        }
-
-}
+  }
 
 }
